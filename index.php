89b384fcf39e7e6971c54f2dd6bc9d33752788d9
Sz4lejesz <sz3lejesz@gmail.com>
2023-01-31 04:02:28 +0100

    pre-finished version, everything working

--- index.php
+++ index.php
@@ -9,19 +9,13 @@ $jsonData = file_get_contents($dataUrl);
 $data = json_decode($jsonData, true);
 
 $teamId = null;
-if (isset($_POST['submit'])) {
+if (isset($_POST['submitTeam']) or isset($_POST['submitPlayer']) or isset($_POST['submitNumber'])) {
     $teamName = $_POST['team'];
     $playerName = $_POST['name'];
     $number = $_POST['number'];
-
     $teams = $data['teams'];
-//    foreach ($teams as $key => $team) {
-//        if ($team['name'] === 'Arsenal') {
-//            echo $key;
-//        }
-//    }
     foreach ($teams as $team) {
-        if ($teamName === $team['name']) {
+        if ($teamName === $team['name']) { // przypisanie nazwy druzyny do numeru
             $teamId = $team['id'];
         }
     }
@@ -34,9 +28,31 @@ if (isset($_POST['submit'])) {
             }
         }
     }
+    $playerFullName = null;
+    $players = $data['elements'];
+    foreach ($players as $player) { // znalezienie gracza
+        if ($playerName === $player['second_name']) {
+            $playerFullName = $player['first_name'] . ' ' . $player['second_name'];
+        }
+    }
+    if (isset($playerFullName)) {
+        foreach ($players as $player) {
+            if ($playerName === $player['second_name']) {
+                $playerTeam = $player['team'];
+                $playerGoals = $player['goals_scored'];
+                $playerAssists = $player['assists'];
+                $playerOwnGoals = $player['own_goals'];
+                $playerYellowCards = $player['yellow_cards'];
+                $playerRedCards = $player['red_cards'];
+            }
+        }
+        foreach ($teams as $team) {
+            if ($playerTeam === $team['id']) {
+                $playerTeam = $team['name'];
+            }
+        }
+    }
 }
-
-
 ?>
 
 <!doctype html>
@@ -66,36 +82,52 @@ if (isset($_POST['submit'])) {
          text-align: center; background: gainsboro" autocomplete="off">
         </div>
         <div class="rectangle">
-            <input type="text" name="name" placeholder="Jesus" style="border: none; width: 196px; height: 50px;
+            <input type="text" name="name" placeholder="Xhaka" style="border: none; width: 196px; height: 50px;
          text-align: center; background: gainsboro" autocomplete="off">
         </div>
         <div class="rectangle">
-            <input type="number" name="number" placeholder="9" style="border: none; width: 196px; height: 50px;
+            <input type="number" name="number" placeholder="34" style="border: none; width: 196px; height: 50px;
          text-align: center; background: gainsboro; -moz-appearance: textfield;" autocomplete="off">
         </div>
         <div class="clearfix"></div>
-        <button type="submit" name="submit" class="rectangle2">Pokaż</button>
-        <button type="submit" name="submit" class="rectangle2">Pokaż</button>
-        <button type="submit" name="submit" class="rectangle2">Pokaż</button>
+        <button type="submit" name="submitTeam" class="rectangle2">Pokaż</button>
+        <button type="submit" name="submitPlayer" class="rectangle2">Pokaż</button>
+        <button type="submit" name="submitNumber" class="rectangle2">Pokaż</button>
     </form>
     <div class="clearfix"></div> <br> <br>
     <div class="filters">Wyniki</div>
     <div class="stripe"></div> <br> <br>
 
     <?php
-    if (!is_null($teamId)) {
-        echo '<div class="filters">' . $teamName . ' - skład' . '</div>' .
-            '<div class="filters">' . 'Wszystkich graczy:' . ' ' . $counter . '</div>' .
-            '<div class="filters">' . 'Bramkarze:' . '</div>';
-        vvv($players, $teamId, 1);
-        echo '<div class="filters">' . 'Obrońcy:' . '</div>';
-        vvv($players, $teamId, 2);
-        echo '<div class="filters">' . 'Pomocnicy:' . '</div>';
-        vvv($players, $teamId, 3);
-        echo '<div class="filters">' . 'Napastnicy:' . '</div>';
-        vvv($players, $teamId, 4);
-    } else {
-        echo 'Nie znaleziono drużyny';
+    if (isset($_POST['submitTeam'])) {
+        if (!is_null($teamId)) {
+            echo '<div class="filters">' . $teamName . ' - skład' . '</div>' .
+                '<div class="filters">' . 'Wszystkich graczy:' . ' ' . $counter . '</div>' .
+                '<div class="filters">' . 'Bramkarze:' . '</div>';
+            vvv($players, $teamId, 1);
+            echo '<div class="filters">' . 'Obrońcy:' . '</div>';
+            vvv($players, $teamId, 2);
+            echo '<div class="filters">' . 'Pomocnicy:' . '</div>';
+            vvv($players, $teamId, 3);
+            echo '<div class="filters">' . 'Napastnicy:' . '</div>';
+            vvv($players, $teamId, 4);
+        } else {
+            echo 'Nie znaleziono drużyny';
+        }
+    }
+    if (isset($_POST['submitPlayer'])) {
+        if (!is_null($playerFullName)) {
+            echo
+                '<div class="filters">' . $playerFullName . '</div>' .
+                'Drużyna: ' . $playerTeam . '<br>' .
+                'Gole: ' . $playerGoals . '<br>' .
+                'Asysty: ' . $playerAssists . '<br>' .
+                'Gole samobójcze: ' . $playerOwnGoals . '<br>' .
+                'Żółte kartki: ' . $playerYellowCards . '<br>' .
+                'Czerwone kartki: ' . $playerRedCards;
+        } else {
+            echo 'Nie znaleziono takiego gracza';
+        }
     }
     ?>
 
